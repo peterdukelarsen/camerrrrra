aeef202bb29b437774bc41446bd878dd088c49ec
peterlarsen <plarsen@umich.edu>
2017-11-05 01:00:55 -0400

    Initial Commit

---
+++ camerrrrra/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  camerrrrra
+//
+//  Created by Peter Larsen on 11/5/17.
+//  Copyright © 2017 Peter Larsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
